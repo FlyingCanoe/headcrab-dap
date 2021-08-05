4b5125606b599061565f6241011c61d22d85557d
FlyingCanoe <flyingcanoe@protonmail.com>
2021-08-05 16:03:44 -0400

    Add log to the example
    In the single session mode of the dap, the IDE start the adapter as a child process and communicate with it using stdin and stdout.
     Since the dev tool is the parent process, it also capture stderr
    
    As such, for debugging purpose, I added the machinery to log to a file.
    
    It is taken strait from rust cookbook.

---
+++ examples/mock.rs
@@ -0,0 +1,27 @@
+fn init_logger() {
+    use log4rs::append::file::FileAppender;
+    use log4rs::config::{Appender, Config, Root};
+    use log4rs::encode::pattern::PatternEncoder;
+
+    let logfile = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
+        .build("output.log")
+        .unwrap();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(
+            Root::builder()
+                .appender("logfile")
+                .build(log::LevelFilter::Info),
+        )
+        .unwrap();
+
+    log4rs::init_config(config).unwrap();
+}
+
+fn main() {
+    init_logger();
+    log::info!("Hello, world!");
+    loop {}
+}
